151b22f81acf5647a441df6606f35902d1c919c6
priyesshrai <priyeshrai369@gmail.com>
2025-05-24 16:59:07 +0530

    feat: add social media links to Hero component for enhanced connectivity

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -21,7 +21,7 @@ export default function Hero() {
                     whileInView={{ opacity: 1, x: 0 }}
                     transition={{ duration: 0.8, ease: 'easeOut' }}
                     viewport={{ once: true }}
-                    className='w-full relative h-full flex flex-col justify-center min-[924px]:py-[90px] min-[924]:ml-[70px] px-[60px] py-[70px] max-[668px]:px-[30px] max-[668px]:py-[40px]'
+                    className='w-full relative h-full flex flex-col justify-center min-[924px]:py-[90px] px-[60px] py-[70px] max-[668px]:px-[30px] max-[668px]:py-[40px]'
                 >
                     <h1 className='lg:text-5xl md:text-3xl text-3xl font-pt font-bold text-primary leading-[1.1]'>
                         Bringing Dreams to Life Your Journey to Parenthood Starts Here!
@@ -39,6 +39,22 @@ export default function Hero() {
                         <Link href='tel:+919138005800' className='px-6 py-[14px] border rounded-md border-primary text-primary font-bold font-roboto'>
                             Call Now
                         </Link>
+                    </div>
+                    <div className='mt-5'>
+                        <p className='mt-5 flex gap-2'>
+                            <Link href='https://www.instagram.com/dr_payalbajaj/' target='_blank' className='w-[50px] h-[50px] bg-white rounded-full p-[2px]'>
+                                <i className="hgi hgi-stroke hgi-instagram w-full h-full bg-primary rounded-full  flex items-center justify-center text-white text-xl"></i>
+                            </Link>
+                            <Link href='https://www.facebook.com/DoctorPayalbajaj' target='_blank' className='w-[50px] h-[50px] bg-white rounded-full p-[2px]'>
+                                <i className="hgi hgi-stroke hgi-facebook-02 w-full h-full bg-primary rounded-full  flex items-center justify-center text-white text-xl"></i>
+                            </Link>
+                            <Link href='https://www.linkedin.com/in/dr-payal-bajaj-2613a923/' target='_blank' className='w-[50px] h-[50px] bg-white rounded-full p-[2px]'>
+                                <i className="hgi hgi-stroke hgi-linkedin-02 w-full h-full bg-primary rounded-full  flex items-center justify-center text-white text-xl"></i>
+                            </Link>
+                            <Link href='https://www.youtube.com/@DrPayalBajaj' target='_blank' className='w-[50px] h-[50px] bg-white rounded-full p-[2px]'>
+                                <i className="hgi hgi-stroke hgi-youtube w-full h-full bg-primary rounded-full  flex items-center justify-center text-white text-xl"></i>
+                            </Link>
+                        </p>
                     </div>
                 </motion.div>
 
