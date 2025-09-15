a5b56a8255d831520571e37cb5841eff1a24dfc9
abhishekgandhi989-blip <abhishekgandhi989@gmail.com>
2025-09-15 15:57:17 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,11 +33,65 @@
 //     </html>
 //   );
 // }
+// import type { Metadata } from "next";
+// import "./globals.css";
+// import Script from "next/script";
+// import SmoothScroll from "./SmoothScroll";
+// import { GoogleAnalytics } from '@next/third-parties/google'
+
+// export const metadata: Metadata = {
+//   title: "Dr. Payal Bajaj - Senior IVF Consultant",
+//   description: "Dr. Payal Bajaj - Senior IVF Consultant",
+// };
+
+// export default function RootLayout({
+//   children,
+// }: Readonly<{
+//   children: React.ReactNode;
+// }>) {
+//   return (
+//     <html lang="en">
+//       <head>
+//         <link rel="shortcut icon" href="/image/icons/logo.png" type="image/x-icon" />
+//         <link rel="stylesheet" href="https://cdn.hugeicons.com/font/hgi-stroke-rounded.css" />
+        
+//         {/* Google Tag Manager */}
+//         <Script id="gtm-head" strategy="beforeInteractive">
+//           {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+//             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+//             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+//             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+//           })(window,document,'script','dataLayer','GTM-5BG85D5G');`}
+//         </Script>
+//         {/* End Google Tag Manager */}
+//       </head>
+//       <body>
+//         {/* Google Tag Manager (noscript) */}
+//         <noscript>
+//           <iframe
+//             src="https://www.googletagmanager.com/ns.html?id=GTM-5BG85D5G"
+//             height="0"
+//             width="0"
+//             style={{ display: "none", visibility: "hidden" }}
+//           ></iframe>
+//         </noscript>
+//         {/* End Google Tag Manager (noscript) */}
+
+//         <SmoothScroll>
+//           {children}
+//         </SmoothScroll>
+
+//         <GoogleAnalytics gaId="G-VX2CY2WZR5" />
+//         <Script src="https://static.elfsight.com/platform/platform.js" async />
+//       </body>
+//     </html>
+//   );
+// }
 import type { Metadata } from "next";
 import "./globals.css";
 import Script from "next/script";
 import SmoothScroll from "./SmoothScroll";
-import { GoogleAnalytics } from '@next/third-parties/google'
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 export const metadata: Metadata = {
   title: "Dr. Payal Bajaj - Senior IVF Consultant",
@@ -52,37 +106,46 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="shortcut icon" href="/image/icons/logo.png" type="image/x-icon" />
-        <link rel="stylesheet" href="https://cdn.hugeicons.com/font/hgi-stroke-rounded.css" />
-        
-        {/* Google Tag Manager */}
+        <link
+          rel="shortcut icon"
+          href="/image/icons/logo.png"
+          type="image/x-icon"
+        />
+        <link
+          rel="stylesheet"
+          href="https://cdn.hugeicons.com/font/hgi-stroke-rounded.css"
+        />
+
+        {/* Google Tag Manager - place this as high in <head> as possible */}
         <Script id="gtm-head" strategy="beforeInteractive">
           {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-5BG85D5G');`}
+          })(window,document,'script','dataLayer','GTM-TNKNSVKR');`}
         </Script>
         {/* End Google Tag Manager */}
       </head>
+
       <body>
-        {/* Google Tag Manager (noscript) */}
+        {/* Google Tag Manager (noscript) - immediately after opening <body> */}
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-5BG85D5G"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TNKNSVKR"
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
-          ></iframe>
+          />
         </noscript>
         {/* End Google Tag Manager (noscript) */}
 
-        <SmoothScroll>
-          {children}
-        </SmoothScroll>
+        <SmoothScroll>{children}</SmoothScroll>
 
-        <GoogleAnalytics gaId="G-VX2CY2WZR5" />
-        <Script src="https://static.elfsight.com/platform/platform.js" async />
+        {/* Third-party script (load lazily) */}
+        <Script
+          src="https://static.elfsight.com/platform/platform.js"
+          strategy="lazyOnload"
+        />
       </body>
     </html>
   );
