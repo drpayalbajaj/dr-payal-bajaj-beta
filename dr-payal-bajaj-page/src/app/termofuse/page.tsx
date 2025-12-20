f1e1827e63e9814937c9ef08815230637afa1569
Abhishek Tiwari <tiwariabhishek9900@gmail.com>
2025-12-20 13:42:54 +0530

    update meta links

--- dr-payal-bajaj-page/src/app/termofuse/page.tsx
+++ dr-payal-bajaj-page/src/app/termofuse/page.tsx
@@ -4,6 +4,7 @@ import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import TermsOfUse from "@/components/TermOfUse";
 
+
 export default function PrivacyPolicyPage() {
   return (
     <>
